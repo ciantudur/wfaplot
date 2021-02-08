b414c1d35229191d2d4f1bdf28f85e29b5ae2f99
Cian Tudur <post@ciantudur.com>
2021-02-08 11:43:54 +0000

    Inital commits
    Initial commit from RStudio

---
+++ R/export_chart.R
@@ -0,0 +1,98 @@
+## -------------------------------------------------------------------------- ##
+## WFAPLOT - EXPORT CHARTS AND ADD LOGO ------------------------------------- ##
+## -------------------------------------------------------------------------- ##
+## R/export_chart.R
+## 08 February 2021
+## Cian Sion (SionC1@cardiff.ac.uk)
+
+
+## REMARKS ---------------------------------------------------------------------
+# This script contains the functions for exporting the charts with and without
+# the WFA logo.
+
+
+## FUNCTION TO ADD LOGO --------------------------------------------------------
+add_logo <- function(plot_path, logo_path, logo_position, logo_scale = 10) {
+  if (!logo_position %in% c(
+    "top right", "top left",
+    "bottom right", "bottom left"
+  )) {
+    stop("Error Message: Uh oh! Logo Position not recognized\nTry:
+         logo_positon = 'top left', 'top right', 'bottom left', 'bottom right'")
+  }
+  
+  plot <- magick::image_read(plot_path)
+  logo_raw <- magick::image_read(logo_path)
+  plot_height <- magick::image_info(plot)$height
+  plot_width <- magick::image_info(plot)$width
+  logo <- magick::image_scale(logo_raw, as.character(plot_width / logo_scale))
+  logo_width <- magick::image_info(logo)$width
+  logo_height <- magick::image_info(logo)$height
+  
+  if (logo_position == "top right") {
+    x_pos <- plot_width - logo_width - 0.01 * plot_width
+    y_pos <- 0.01 * plot_height
+  } else if (logo_position == "top left") {
+    x_pos <- 0.01 * plot_width
+    y_pos <- 0.01 * plot_height
+  } else if (logo_position == "bottom right") {
+    x_pos <- plot_width - logo_width - 0.01 * plot_width
+    y_pos <- plot_height - logo_height - 0.01 * plot_height
+  } else if (logo_position == "bottom left") {
+    x_pos <- 0.01 * plot_width
+    y_pos <- plot_height - logo_height - 0.01 * plot_height
+  }
+  
+  magick::image_composite(plot, logo, offset = paste0("+", x_pos, "+", y_pos))
+}
+
+
+# Function to export chart
+export_chart <- function(chart_name, file_name, include_logo = c(T, F),
+                         export_width = NULL, export_height = NULL,
+                         export_res = NULL) { 
+  if (include_logo == T) {
+    if (is.null(export_width)) {
+      export_width <- 6.22222
+    } else {}
+    if (is.null(export_height)) {
+      export_height <- 3.5
+    } else {}
+    if (is.null(export_res)) {
+      export_res <- 900
+    } else {}
+    
+    png(as.character(file_name), units = "in", width = export_width,
+        height = export_height, res = export_res) 
+    plot(chart_name)
+    dev.off()
+    
+    plot_with_logo <- add_logo(
+      plot_path = as.character(file_name),
+      logo_path = "wfalogo.png",
+      logo_position = "top right",
+      logo_scale = 7.5)
+    magick::image_write(plot_with_logo, as.character(file_name))
+  } else if (include_logo == F) {
+    if (is.null(export_width)) {
+      export_width <- 6
+    } else {}
+    if (is.null(export_height)) {
+      export_height <- 3
+    } else {}
+    if (is.null(export_res)) {
+      export_res <- 900
+    } else {}
+    
+    chart_name <- chart_name +
+      theme(
+        plot.title = element_blank(),
+        plot.subtitle = element_blank(),
+        plot.caption = element_blank())
+    
+    png(as.character(file_name), units = "in", width = export_width,
+        height = export_height, res = export_res) 
+    plot(chart_name)
+    dev.off()
+  } 
+}
---
+++ R/theme_wfa.R
@@ -0,0 +1,47 @@
+## -------------------------------------------------------------------------- ##
+## WFAPLOT - SET UP GGPLOT THEME -------------------------------------------- ##
+## -------------------------------------------------------------------------- ##
+## R/theme_wfa.R
+## 08 February 2021
+## Cian Sion (SionC1@cardiff.ac.uk)
+
+
+## REMARKS ---------------------------------------------------------------------
+# This script contains the paramaters for theme_wfa() to be called when
+# generating ggplots.
+
+
+theme_wfa<- function(){
+  library(firatheme)
+  theme_minimal() %+replace%
+    theme(
+      axis.text = element_text(
+        color = "#373737",
+        family = "Fira Sans"
+      ),
+      axis.text.x.bottom = element_text(
+        angle = 90,
+        hjust = 1,
+        vjust = 0.5
+      ),
+      axis.title.y = element_text(
+        colour = "#373737",
+        family = "Fira Sans",
+        size = 8
+      ),
+      axis.title.x = element_text(
+        colour = "#373737",
+        family = "Fira Sans",
+        size = 9
+      ),
+      legend.title = element_blank(),
+      legend.text = element_text(family = "Fira Sans"),
+      plot.title = element_text(family = "Fira Sans Medium"),
+      plot.subtitle = element_text(family = "Fira Sans", color = "#373737"),
+      plot.caption = element_text(
+        size = 8,
+        hjust = 0,
+        face = "italic",
+        family = "Fira Sans",
+      ), complete = T)
+}
